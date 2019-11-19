2e8936206480aa297054df83dd2c4cdd75afcf0a
Matthew Brown <matt.brown@cambridgeconsultants.com>
2019-11-19 17:32:37 +0000

    Remove semicolons

--- js/background.js
+++ js/background.js
@@ -2,30 +2,30 @@
 console.log('hello!')
 
 // three different variables for the different images
-let backgrounds = ['mike-yukhtenko', 'gabriele-garanzelli', 'fabian-oelkers'];
+let backgrounds = ['mike-yukhtenko', 'gabriele-garanzelli', 'fabian-oelkers']
 
-let button = document.querySelector('button');
+let button = document.querySelector('button')
 // when the button is clicked it triggers a function
 button.addEventListener('click', function () {
   // 1. log to the console that you have clicked the button
   console.log('I was clicked!')
 
   // this selects a randon background from above and stores it as the variable 'randomBackground'
-  let randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  let randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
   // this creates a string that css will understand to be a background url
   let imageUrl = "url('./images/" + randomBackground + ".jpg')"
 
 
   // 2. change the image url of the body background to the new one saved above
-  let body = document.querySelector('body');
-  body.style.background = imageUrl;
+  let body = document.querySelector('body')
+  body.style.background = imageUrl
 
   // EXTENSION TASK!
   // 3. Change the text of the 'span' to be that of the photographer
   //   (hint, you should re-use randomBackground)
 
   // Double points if you replace '-' between their names with a space
-  let artist = randomBackground.replace(/-/g, ' ');
-  let span = document.querySelector('span');
-  span.innerHTML = artist;
+  let artist = randomBackground.replace(/-/g, ' ')
+  let span = document.querySelector('span')
+  span.innerHTML = artist
 })
