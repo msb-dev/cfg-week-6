2ec0dfc25232587f5111e45a3f6356ff63efc10e
Matthew Brown <matt.brown@cambridgeconsultants.com>
2019-11-19 17:30:58 +0000

    Remove solution from master

--- js/background.js
+++ js/background.js
@@ -8,7 +8,7 @@ let button = document.querySelector('button');
 // when the button is clicked it triggers a function
 button.addEventListener('click', function () {
   // 1. log to the console that you have clicked the button
-  console.log('I was clicked!')
+
 
   // this selects a randon background from above and stores it as the variable 'randomBackground'
   let randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
@@ -17,15 +17,10 @@ button.addEventListener('click', function () {
 
 
   // 2. change the image url of the body background to the new one saved above
-  let body = document.querySelector('body');
-  body.style.background = imageUrl;
 
   // EXTENSION TASK!
   // 3. Change the text of the 'span' to be that of the photographer
   //   (hint, you should re-use randomBackground)
 
   // Double points if you replace '-' between their names with a space
-  let artist = randomBackground.replace(/-/g, ' ');
-  let span = document.querySelector('span');
-  span.innerHTML = artist;
 })
